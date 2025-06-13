9bd87983526e47daadf7273f00eca061569dee55
natasyapurnamaa <npurnama51@gmail.com>
2025-06-13 20:55:54 +0700

    connect API to event calendar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ import Transaction from "./components/user/Transaction";
 import DashboardLayout from "./components/user/dashboard/dashboardLayout";
 import Notifikasi from "./components/user/dashboard/Notifikasi";
 import Profile from "./components/user/dashboard/Profile";
-import Transaksi from "./components/user/dashboard/transaksi";
+import Transaksi from "./components/user/dashboard/Transaksi";
 
 // Admin
 import Dashboard from "./pages/admin/dashboard";
